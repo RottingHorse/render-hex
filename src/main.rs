3150b62aae5112f2596e264ec2df547af0b91129
RottingHorse <minichev.s.l@gmail.com>
2023-07-04 13:11:14 +0300

    implemented parse (app not works yet)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::env;
 use svg::Document;
 use svg::node::element::path::Command;
+use crate::Operation::{Forvard, Home, Noop, TurnLeft, TurnRight};
+
+const WIDTH: isize = 400;
+const HEIGHT: isize = WIDTH;
 
 #[derive(Debug, Clone, Copy)]
 enum Operation {
@@ -33,5 +37,19 @@ fn convert(operations: &Vec<Operation>) -> Vec<Command> {
 }
 
 fn parse(input: &str) -> Vec<Operation> {
-    unimplemented!()
+    let mut steps = Vec::<Operation>::new();
+    for byte in input.bytes() {
+        let step = match byte {
+            b'0' => Home,
+            b'1'..=b'9' => {
+                let distance = {byte - 0x30} as isize;
+                Forvard(distance * (HEIGHT/10))
+            },
+            b'a'| b'b'|b'c' => TurnLeft,
+            b'd'|b'e'|b'f' => TurnRight,
+            _ => Noop(byte as usize),
+        };
+        steps.push(step);
+    }
+    steps
 }
